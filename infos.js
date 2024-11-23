62e2eda9e2d425f400fe9d8063c0fb9610a753cc
JuZab <jzabiolle@hotmail.fr>
2024-11-23 16:35:59 +0100

    ajout fichier infos.js. Création du bloc infos avec titres et changement d'images au clic.

---
+++ infos.js
@@ -0,0 +1,29 @@
+
+let boutonTime = document.getElementById("btn-time")
+    boutonTime.addEventListener("click", () =>{
+    let imgInfosTime = document.getElementById("img-infos")
+    document.getElementById("img-infos").className=""
+    imgInfosTime.classList.add("img-infos-time","img-infos-style")
+
+})
+let boutonCours = document.getElementById("btn-cours")
+    boutonCours.addEventListener("click", () =>{
+    let imgInfosCours = document.getElementById("img-infos")
+    document.getElementById("img-infos").className=""
+    imgInfosCours.classList.add("img-infos-cours","img-infos-style")
+
+})
+let boutonSecurite = document.getElementById("btn-securite")
+    boutonSecurite.addEventListener("click", () =>{
+    let imgInfosSecurite = document.getElementById("img-infos")
+    document.getElementById("img-infos").className=""
+    imgInfosSecurite.classList.add("img-infos-securite","img-infos-style")
+
+})
+let boutonTarifs = document.getElementById("btn-tarifs")
+    boutonTarifs.addEventListener("click", () =>{
+    let imgInfosTarifs = document.getElementById("img-infos")
+    document.getElementById("img-infos").className=""
+    imgInfosTarifs.classList.add("img-infos-tarifs","img-infos-style")
+})
+    
